10d498a14eeffce97276697c3163c9beb90ab87b
Dong <690443961@qq.com>
2019-04-03 19:48:43 +0800

    first

---
+++ app/src/main/java/com/example/apriltagstest_2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.apriltagstest_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+}
